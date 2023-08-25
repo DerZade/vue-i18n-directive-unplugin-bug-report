0b58caf4b753246253b0c5e61f32aa1dcf0148cd
DerZade <DerZade@gmail.com>
2023-08-25 11:18:10 +0200

    install vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+    locale: 'en',
+    messages: {
+        en: {
+            global: "Global Path works!"
+        }
+    }
+})
+
+createApp(App).use(i18n).mount('#app')
